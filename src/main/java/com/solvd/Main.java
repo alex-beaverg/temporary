256506780c43fe9254aca5211df79c31ac5aa5b5
Alexey Bobrikov <a.beaverg@gmail.com>
2023-11-21 13:31:14 +0300

    after merge

--- src/main/java/com/solvd/Main.java
+++ src/main/java/com/solvd/Main.java
@@ -3,7 +3,7 @@ package com.solvd;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        Person person = new Person("Alex", 35, "Hill");
+        Person person = new Person("Alex", "Hill", 35);
         System.out.println(person);
     }
 }
